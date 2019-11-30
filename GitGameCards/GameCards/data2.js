f18861306ff5ee0c9687a8f76d72c07c2d06e923
Yunnuenmontes <52986128+Yunnuenmontes@users.noreply.github.com>
2019-11-30 10:16:22 -0600

    Add files via upload

---
+++ GitGameCards/GameCards/data2.js
@@ -0,0 +1,46 @@
+let MisJuegos={
+  "Caricachupas": {
+    desc:"Consta de una canción grupal por turnos ejemplo “jugador 1 dice :caricachupas, jugador 2 dice :presenta,jugador 3 dice : nombres de …” y el siguiente jugador elige una categoría de lo que sea que te imagines . Ejemplo: Marcas de cualquier categoría, marcas de autos,animales,marcas de cigarro,etc… ( y asi sucesivamente , no importa la cantidad de jugadores),cada jugador tendrá que decir, como el juego lo menciona, un nombre del tema que se haya elegido, ejemplo … MARCAS DE CIGARROS, cada jugador deberá decir una marca de cigarros, aquel que ya no pueda mencionar o no sepa otra categoría aparte de las mencionadas perderá (no se pueden repetir). ",
+    alert:"La categoría para jugar será (Lenguajes de programación). El jugador que pierda tomará de fondo 1 cerveza",
+  },
+"juegoA":{desc:"AQUI VA DESCRIPCION JUEGO A"},
+"Todos toman": {
+  desc:"En este juego todo los jugadores deben de beber",
+  alert:""
+}
+}
+let Retos={
+  "Peliculas": "en este evento, el total de los jugadores que hay en el lugar tendrán que dividirse en dos equipos o más (dependiendo de la cantidad y de cómo elijan los jugadores para ser equivalentes), antes de comenzar el juego , cada equipo deberá coordinarse para elegir a un jugador (por turno) para ser el 'actor' de la película que los otros equipos determinaran, está actuación debe ser adivinada por el resto del equipo al que pertenece el 'actor', hay dos maneras de perder en este juego, una de ellas es 1: si el equipo adivina la película los demás equipos deberán igualar los aciertos o irán perdiendo conforme el juego avanza, al final del juego el equipo que menos puntaje tenga pierde. O 2: si el equipo adivina la película, los demás equipos perderán y deberán tener un castigo por turno cada vez que no acierten o cada vez que el equipo contrario adivine su película",
+  "Compadre": "el jugador que obtenga este beneficio en el juego, escogerá a otro jugador, el que el prefiera, para cada que pierda o tome, éste compadre lo acompañe en su pena y haga lo mismo que él",
+  "Esclavo":"el jugador que obtenga esta categoría, será el menos afortunado, y tendrá que ser el esclavo del grupo de juego, tendrá que hacer lo que le digan sus compañeros sin negarse, ejemplo servir una cuba, o prender un cigarro o ir por cosas, el tiempo se determina por los jugadores (por rondas) o hasta que l salga esta categoría a otro jugador ",
+  "Disparejo":" juego con el pulgar en grupo, direccionándolo hacia arriba o hacia abajo, aquel que coloque su pulgar en dirección diferente a los demás, será descartado del juego, cuando queden solo dos personas pasaran al siguiente juego que es piedra papel o tijeras…",
+  "Piedra papel o tijeras 2 de 3": "consiste en un juego de 2 los cuales deben hacer figuras con una mano de PIEDRA (el puño cerrado), PAPEL (mano extendida) o TIJERA (dos dedos simulando unas tijeras), las reglas son: piedra aplasta a tijeras, papel cubre a piedra,  tijeras corta a papel, aquel que pierda en las 3 rondas (2 veces) tendrá el castigo.",
+  "Regla": "la persona que obtenga este beneficio, podrá poner una regla, la que el prefiera y durará hasta que otro jugador obtenga este beneficio",
+  "Rima": "el juego consiste en que el jugador en turno diga una palabra, la que él quiera, la idea es que el siguiente jugador debe decir una palabra que rime con la palabra que el jugador anterior dijo, o en todo caso que la palabra que vaya a mencionar este relacionada, el jugador que no logre rimar su palabra o que no este relacionada, perderá ",
+  "Palabras":" el juego consiste en que el jugador en turno diga una palabra, la que se le ocurra, y el siguiente jugador diga una palabra que inicie con las dos ultimas letras de la palabra anterior, ejemplo : PALETA, el siuiente jugador debe decir una palabra que inicie con TA, ejemplo TAMARINDO, el siguiente jugador debe decir una palabra que inicie con DO, y asi sucesivamente, aquel que no logre decir una palabra con esa secuencia o se equivoque pierde (el juego sigue las reglas de la gramática, ejemplo : merengue  no son dos letras sino 3 pero no se pueden separar para armar otra palabra: gue:  Guerrero*",
+  "mímica": "el juego es casi igual a películas pero en este caso el equipo escogerá al actor para actuar algún personaje o actor o cosa, las reglas de película son las mismas "
+}
+
+
+let Map={
+  'A':{key:"Todos toman"},
+  '2':{key:"juegoA"},
+  '3':{key:"juegoA"},
+  '4':{key:"juegoA"},
+  '5':{key:"Caricachupas"},
+  '6':{key:"juegoA"},
+  '7':{key:"juegoA"},
+  '8':{key:"juegoA"},
+  '9':{key:"juegoA"},
+  '10':{key:"juegoA"},
+  'J':{key:"juegoA"},
+  'Q':{key:"juegoA"},
+  'k':{key:"juegoA"}
+}
+
+let imgCard=[
+  {color:'red',palo:'corazon'},
+  {color:'black',palo:'picas'},
+  {color:'red',palo:'diamante'},
+  {color:'black',palo:'trebol'}
+]
